0e4b452d4d4eed38720b1d5faf669883effa71e2
Habeeb rahman <habeebrahman6103@gmail.com>
2025-06-30 21:09:53 +0530

    added loading skeleton

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -8,7 +8,10 @@ import cloudinary from "../config/cloudinary.js";
 export const getUsers = async(req,res)=>{
     try {
       const loggedInUserId=req.user._id
-      const filteredUsers=await User.find({_id:{$ne:loggedInUserId}}).select("-password")
+      const filteredUsers = await User.find({
+        _id: { $ne: loggedInUserId },
+        verified: true 
+      }).select("-password");
       res.status(200).json(filteredUsers)
     } catch (error) {
       console.log("Error in getUsers: ",error.message) 
